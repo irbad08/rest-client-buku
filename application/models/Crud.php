1d1f286ca83bf2413df6fc022ca721200c06e9dc
irbad08 <56690480+irbad08@users.noreply.github.com>
2020-12-30 21:29:33 +0700

    first commit

---
+++ application/models/Crud.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
+class Crud extends CI_Model
+{
+
+
+    private $_client;
+
+    public function __construct()
+    {
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost/resfull-server-buku/'
+        ]);
+    }
+
+    // List all your items
+    public function read()
+    {
+
+        $response = $this->_client->request('GET');
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result['data'];
+    }
+
+    // Add a new item
+    public function add()
+    {
+        
+    }
+
+    //Update one item
+    public function update()
+    {
+    }
+
+    //Delete one item
+    public function delete()
+    {
+    }
+}
+
+/* End of file Controllername.php */
